2f6a8d3ff3bfaebcbaf5a26e3ccdf9a3ce37ed8b
Jorge <jbernal0019@yahoo.es>
2019-07-18 16:36:55 -0400

    Add tests for the update and delete operations on PipelineInstanceDetailViewTests.

--- chris_backend/pipelineinstances/tests/test_views.py
+++ chris_backend/pipelineinstances/tests/test_views.py
@@ -15,7 +15,6 @@ from plugins.models import PluginParameter, DefaultPathParameter, DefaultIntPara
 from plugininstances.models import PluginInstance
 from pipelines.models import Pipeline, PluginPiping
 from pipelineinstances.models import PipelineInstance
-from pipelineinstances import views
 
 
 class ViewTests(TestCase):
@@ -34,6 +33,7 @@ class ViewTests(TestCase):
                                                   'default': 111111}}
         self.username = 'foo'
         self.password = 'foo-pass'
+
         (self.compute_resource, tf) = ComputeResource.objects.get_or_create(
             compute_resource_identifier="host")
 
@@ -178,16 +178,54 @@ class PipelineInstanceDetailViewTests(ViewTests):
         (pipeline_inst, tf) = PipelineInstance.objects.get_or_create(title="PipelineInst1",
                                                                      pipeline=pipeline,
                                                                      owner=owner)
-        self.read_url = reverse("pipelineinstance-detail", kwargs={"pk": pipeline_inst.id})
+        self.read_update_delete_url = reverse("pipelineinstance-detail", kwargs={"pk": pipeline_inst.id})
 
     def test_pipeline_instance_detail_success(self):
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.read_url)
+        response = self.client.get(self.read_update_delete_url)
         self.assertContains(response, "PipelineInst1")
 
     def test_pipeline_instance_detail_failure_unauthenticated(self):
-        response = self.client.get(self.read_url)
+        response = self.client.get(self.read_update_delete_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_pipeline_instance_update_success(self):
+        put = json.dumps({
+            "template": {"data": [{"name": "title", "value": "Test pipeline instance"}]}})
+
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.put(self.read_update_delete_url, data=put,
+                                   content_type=self.content_type)
+        self.assertContains(response, "Test pipeline instance")
+
+    def test_pipeline_instance_update_failure_unauthenticated(self):
+        response = self.client.put(self.read_update_delete_url, data={},
+                                   content_type=self.content_type)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_pipeline_instance_update_failure_access_denied(self):
+        put = json.dumps({
+            "template": {"data": [{"name": "title", "value": "Test pipeline instance"}]}})
+
+        self.client.login(username=self.other_username, password=self.other_password)
+        response = self.client.put(self.read_update_delete_url, data=put,
+                                   content_type=self.content_type)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_pipeline_instance_delete_success(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.delete(self.read_update_delete_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(PipelineInstance.objects.count(), 0)
+
+    def test_pipeline_instance_delete_failure_unauthenticated(self):
+        response = self.client.delete(self.read_update_delete_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_pipeline_instance_delete_failure_access_denied(self):
+        self.client.login(username=self.other_username, password=self.other_password)
+        response = self.client.delete(self.read_update_delete_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 class PipelineInstancePluginInstanceListViewTests(ViewTests):
